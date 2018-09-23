70f5b065c37eb4f71a6f7c875c5a044ade759677
DzmitryMashuk <d.mashuk@itransition.com>
2018-09-24 01:43:40 +0300

    Adding method check.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pair = [];
+  
+  bracketsConfig.forEach(function(value) {
+    pair.push(value[0] + value[1]);
+  });
+
+  for (let i = 0; i < pair.length; i++) {
+    let index = str.indexOf(pair[i]);
+    if (-1 !== index) {
+      str = str.replace(str.substring(index, index + 2), "");
+      i   = -1;
+    }
+  }
+
+  if (0 === str.length) {
+    return true;
+  }
+
+  return false;
 }
